09020c0b321a92e11fe539dc0c2628c1f77afec4
Vivian C <baongocche@gmail.com>
2024-03-08 11:47:19 -0800

    updaates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
 
+  const [style, setStyle] = useState(false)
 
-  const handleClick = () => {
+  const noButton = document.querySelector('#no-bttn');
+  var x = Math.random() * (window.innerWidth - noButton?.offsetWidth);
+  var y = Math.random() * (window.innerHeight - noButton?.offsetHeight);
 
-  }
+  useEffect(() => {
 
+  }, [style]);
+
+    const moveNoButton = () => {
+      setStyle(true)
+    }
 
   return (
     <div className="App">
@@ -30,9 +38,15 @@ function App() {
 
         <div className="choicetwo">
           <button
-            onClick={handleClick}
+            onClick={moveNoButton()}
+            onmouseover={moveNoButton()}
             className="bttn"
-            id="no-bttn">
+            id="no-bttn"
+            style ={{
+              left: style ? ` ${x}px` : "",
+              top: style ? `${y}px` : "",
+              position: style ? "absolute" : ""
+            }}>
             <svg class="no-heart" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2 c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
             </svg>
           </button>
